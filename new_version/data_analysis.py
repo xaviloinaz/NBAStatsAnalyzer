d0027ac2ac63e3fe08714a8dd72e2f1b500ef8e1
Xavi Loinaz <x.e.loinaz@gmail.com>
2020-04-24 18:41:17 -0700

    first commit

---
+++ new_version/data_analysis.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import pickle
+import os
+import matplotlib.pyplot as plt
+import scipy
+import statsmodels.api as sm
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+
+print("Current directory:")
+print(os.getcwd())
+os.chdir(os.path.expanduser("~"))
+print(os.getcwd())
+
+
+years = list(range(1980,2021))
+years_stats_dict = {}
+for year_index in range(0, len(years)):
+    with open('Desktop/Coding/Python/BasketballStatsAnalysis/new_version/data/player_stats_' + str(years[year_index]) + '.pkl', 'rb') as f:
+        data = pickle.load(f)
+        years_stats_dict[years[year_index]] = data
+
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+pd.options.display.width=None
+
+merged_2019_and_2020 = years_stats_dict[2019].merge(years_stats_dict[2020], left_on='playerID', right_on='playerID',
+          suffixes=('_2019', '_2020'))
+
+
+
+filtered_empties_2019_and_2020 = merged_2019_and_2020[merged_2019_and_2020.FGA_totals_2019 != str(0)]
+filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FGA_totals_2020 != str(0)]
+filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2019 != str(0)]
+filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2020 != str(0)]
+
+filtered_empties_2019_and_2020['FG_totals_2019'] = filtered_empties_2019_and_2020['FG_totals_2019'].astype(float)
+filtered_empties_2019_and_2020['FG_totals_2020'] = filtered_empties_2019_and_2020['FG_totals_2020'].astype(float)
+filtered_empties_2019_and_2020['TS%_2019'] = filtered_empties_2019_and_2020['TS%_2019'].astype(float)
+filtered_empties_2019_and_2020['TS%_2020'] = filtered_empties_2019_and_2020['TS%_2020'].astype(float)
+filtered_empties_2019_and_2020['FG%_totals_2019'] = filtered_empties_2019_and_2020['FG%_totals_2019'].astype(float)
+filtered_empties_2019_and_2020['FG%_totals_2020'] = filtered_empties_2019_and_2020['FG%_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['3P%_totals_2019'] = filtered_empties_2019_and_2020['3P%_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['3P%_totals_2020'] = filtered_empties_2019_and_2020['3P%_totals_2020'].astype(float)
+filtered_empties_2019_and_2020['2P%_totals_2019'] = filtered_empties_2019_and_2020['2P%_totals_2019'].astype(float)
+filtered_empties_2019_and_2020['2P%_totals_2020'] = filtered_empties_2019_and_2020['2P%_totals_2020'].astype(float)
+filtered_empties_2019_and_2020['FT%_totals_2019'] = filtered_empties_2019_and_2020['FT%_totals_2019'].astype(float)
+filtered_empties_2019_and_2020['FT%_totals_2020'] = filtered_empties_2019_and_2020['FT%_totals_2020'].astype(float)
+filtered_empties_2019_and_2020['3PAr_2019'] = filtered_empties_2019_and_2020['3PAr_2019'].astype(float)
+filtered_empties_2019_and_2020['3PAr_2020'] = filtered_empties_2019_and_2020['3PAr_2020'].astype(float)
+filtered_empties_2019_and_2020['FTr_2019'] = filtered_empties_2019_and_2020['FTr_2019'].astype(float)
+filtered_empties_2019_and_2020['FTr_2020'] = filtered_empties_2019_and_2020['FTr_2020'].astype(float)
+filtered_empties_2019_and_2020['USG%_2019'] = filtered_empties_2019_and_2020['USG%_2019'].astype(float)
+filtered_empties_2019_and_2020['USG%_2020'] = filtered_empties_2019_and_2020['USG%_2020'].astype(float)
+filtered_empties_2019_and_2020['eFG%_totals_2019'] = filtered_empties_2019_and_2020['eFG%_totals_2019'].astype(float)
+filtered_empties_2019_and_2020['eFG%_totals_2020'] = filtered_empties_2019_and_2020['eFG%_totals_2020'].astype(float)
+filtered_empties_2019_and_2020['WS/48_2019'] = filtered_empties_2019_and_2020['WS/48_2019'].astype(float)
+filtered_empties_2019_and_2020['WS/48_2020'] = filtered_empties_2019_and_2020['WS/48_2020'].astype(float)
+filtered_empties_2019_and_2020['OWS_2019'] = filtered_empties_2019_and_2020['OWS_2019'].astype(float)
+filtered_empties_2019_and_2020['OWS_2020'] = filtered_empties_2019_and_2020['OWS_2020'].astype(float)
+
+
+
+filtered_by_FG_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FG_totals_2019 >= 200]
+filtered_by_FG_2019_and_2020 = filtered_by_FG_2019_and_2020[filtered_by_FG_2019_and_2020.FG_totals_2020 >= 150]
+
+print(filtered_by_FG_2019_and_2020.head(10))
+print("shape of data: ", filtered_by_FG_2019_and_2020.shape)
+
+# xlabel = 'eFG%_totals_2019'
+# ylabel = 'TS%_2019'
+xlabel = 'USG%_2020'
+ylabel = 'OWS_2020'
+
+
+model = sm.OLS(filtered_by_FG_2019_and_2020[ylabel], sm.add_constant(np.column_stack((filtered_by_FG_2019_and_2020['3PAr_2019'], filtered_by_FG_2019_and_2020['FTr_2019'], filtered_by_FG_2019_and_2020['2P%_totals_2019'], filtered_by_FG_2019_and_2020['FT%_totals_2019']))))
+results = model.fit()
+print(results.summary())
+
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(filtered_by_FG_2019_and_2020[xlabel], filtered_by_FG_2019_and_2020[ylabel])
+print("r_value: ", r_value)
+print("r_squared_value: ", r_value ** 2)
+
+filtered_by_FG_2019_and_2020.plot(x=xlabel, y=ylabel, style='o')
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.show()
---
+++ new_version/scrape_and_format_data.py
@@ -0,0 +1,77 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+stats_for_each_year = []
+years = list(range(1980, 2021))
+for year in years:
+    url_totals = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_totals.html"
+    url_per_game = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    url_per_36_min = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_minute.html"
+    url_per_100_poss = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_poss.html"
+    url_advanced = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_advanced.html"
+    urls = [url_totals, url_per_game, url_per_36_min, url_per_100_poss, url_advanced]
+
+    url_index = 0
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        all_tr = soup.findAll("tr")
+        header_data = all_tr[0]
+        players_data = []
+        player_ids = []
+        for i in range(0, len(all_tr)):
+            # if all_tr[i].get("class") == ['full_table'] or all_tr[i].get("class") == ['italic_text', 'partial_table']:
+            # ^ The above if statement can be used if we care about partial seasons
+            if all_tr[i].get("class") == ['full_table']:
+                players_data.append(all_tr[i])
+                player_id = all_tr[i].findAll('td')[0].get("data-append-csv")
+                player_ids.append(player_id)
+
+        # I'm getting rid of the Rk column below with [1:]:
+        headers = ["playerID"] + [th.getText() for th in header_data.findAll('th')][1:]
+        players_stats = [[player_ids[i]] + [td.getText() for td in players_data[i].findAll('td')] \
+                         for i in range(len(players_data))]
+
+        if url_index == 0:
+            stats_totals = pd.DataFrame(players_stats, columns=headers)
+        elif url_index == 1:
+            stats_per_game = pd.DataFrame(players_stats, columns=headers)
+        elif url_index == 2:
+            stats_per_36_min = pd.DataFrame(players_stats, columns=headers)
+        elif url_index == 3:
+            stats_per_100_poss = pd.DataFrame(players_stats, columns=headers)
+        else:
+            stats_advanced = pd.DataFrame(players_stats, columns=headers)
+
+            # Join the dataframes here
+            all_player_stats = stats_totals.join(stats_per_game, lsuffix='_totals')\
+                .join(stats_per_36_min, lsuffix='_per_game')\
+                .join(stats_per_100_poss, lsuffix='_per_36_min', rsuffix='_per_100_poss')\
+                .join(stats_advanced, rsuffix='_advanced')
+            # Drop extraneous columns:
+            all_player_stats = all_player_stats\
+                .drop(columns=['playerID_totals', 'playerID_per_game', 'playerID_per_36_min', 'playerID_per_100_poss'])\
+                .drop(columns=['Player_totals', 'Player_per_game', 'Player_per_36_min', 'Player_per_100_poss']) \
+                .drop(columns=['Pos_totals', 'Pos_per_game', 'Pos_per_36_min', 'Pos_per_100_poss']) \
+                .drop(columns=['Age_totals', 'Age_per_game', 'Age_per_36_min', 'Age_per_100_poss']) \
+                .drop(columns=['Tm_totals', 'Tm_per_game', 'Tm_per_36_min', 'Tm_per_100_poss']) \
+                .drop(columns=['G_totals', 'G_per_game', 'G_per_36_min', 'G_per_100_poss']) \
+                .drop(columns=['GS_totals', 'GS_per_game', 'GS_per_36_min', 'GS_per_100_poss'])
+
+        url_index += 1
+
+    pd.set_option("display.max_rows", 10, "display.max_columns", None)
+    stats_for_each_year.append(all_player_stats)
+    # print(all_player_stats.head(10))
+
+years_stats_dict = {}
+for year_index in range(0, len(years)):
+    years_stats_dict[years[year_index]] = stats_for_each_year[year_index]
+    stats_for_each_year[year_index].to_pickle("./data/player_stats_" + str(years[year_index]) + ".pkl")
+
+print(years_stats_dict)
+
