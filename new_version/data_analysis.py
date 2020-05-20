6538890f1e3b5189717a13ea69f49bbff5ad6abb
Xavi Loinaz <x.e.loinaz@gmail.com>
2020-05-20 16:53:50 -0700

    i can now compare any two years

--- new_version/data_analysis.py
+++ new_version/data_analysis.py
@@ -23,66 +23,111 @@ for year_index in range(0, len(years)):
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 pd.options.display.width=None
 
-merged_2019_and_2020 = years_stats_dict[2019].merge(years_stats_dict[2020], left_on='playerID', right_on='playerID',
-          suffixes=('_2019', '_2020'))
 
 
 
-filtered_empties_2019_and_2020 = merged_2019_and_2020[merged_2019_and_2020.FGA_totals_2019 != str(0)]
-filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FGA_totals_2020 != str(0)]
-filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2019 != str(0)]
-filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2020 != str(0)]
+def merged_years_setup(year1, year2, minFG_year1, minFG_year2): # The input years are ints
+    merged = years_stats_dict[year1].merge(years_stats_dict[year2], left_on='playerID', right_on='playerID',
+          suffixes=('_' + str(year1), '_' + str(year2)))
+    filtered_empties = merged[merged["FGA_totals_" + str(year1)] != str(0)]
+    filtered_empties = filtered_empties[filtered_empties["FGA_totals_" + str(year2)] != str(0)]
+    filtered_empties = filtered_empties[filtered_empties["FTA_totals_" + str(year1)] != str(0)]
+    filtered_empties = filtered_empties[filtered_empties["FTA_totals_" + str(year2)] != str(0)]
+    filtered_empties = filtered_empties[filtered_empties["2PA_totals_" + str(year1)] != str(0)]
+    filtered_empties = filtered_empties[filtered_empties["2PA_totals_" + str(year2)] != str(0)]
 
-filtered_empties_2019_and_2020['FG_totals_2019'] = filtered_empties_2019_and_2020['FG_totals_2019'].astype(float)
-filtered_empties_2019_and_2020['FG_totals_2020'] = filtered_empties_2019_and_2020['FG_totals_2020'].astype(float)
-filtered_empties_2019_and_2020['TS%_2019'] = filtered_empties_2019_and_2020['TS%_2019'].astype(float)
-filtered_empties_2019_and_2020['TS%_2020'] = filtered_empties_2019_and_2020['TS%_2020'].astype(float)
-filtered_empties_2019_and_2020['FG%_totals_2019'] = filtered_empties_2019_and_2020['FG%_totals_2019'].astype(float)
-filtered_empties_2019_and_2020['FG%_totals_2020'] = filtered_empties_2019_and_2020['FG%_totals_2020'].astype(float)
-# filtered_empties_2019_and_2020['3P%_totals_2019'] = filtered_empties_2019_and_2020['3P%_totals_2019'].astype(float)
-# filtered_empties_2019_and_2020['3P%_totals_2020'] = filtered_empties_2019_and_2020['3P%_totals_2020'].astype(float)
-filtered_empties_2019_and_2020['2P%_totals_2019'] = filtered_empties_2019_and_2020['2P%_totals_2019'].astype(float)
-filtered_empties_2019_and_2020['2P%_totals_2020'] = filtered_empties_2019_and_2020['2P%_totals_2020'].astype(float)
-filtered_empties_2019_and_2020['FT%_totals_2019'] = filtered_empties_2019_and_2020['FT%_totals_2019'].astype(float)
-filtered_empties_2019_and_2020['FT%_totals_2020'] = filtered_empties_2019_and_2020['FT%_totals_2020'].astype(float)
-filtered_empties_2019_and_2020['3PAr_2019'] = filtered_empties_2019_and_2020['3PAr_2019'].astype(float)
-filtered_empties_2019_and_2020['3PAr_2020'] = filtered_empties_2019_and_2020['3PAr_2020'].astype(float)
-filtered_empties_2019_and_2020['FTr_2019'] = filtered_empties_2019_and_2020['FTr_2019'].astype(float)
-filtered_empties_2019_and_2020['FTr_2020'] = filtered_empties_2019_and_2020['FTr_2020'].astype(float)
-filtered_empties_2019_and_2020['USG%_2019'] = filtered_empties_2019_and_2020['USG%_2019'].astype(float)
-filtered_empties_2019_and_2020['USG%_2020'] = filtered_empties_2019_and_2020['USG%_2020'].astype(float)
-filtered_empties_2019_and_2020['eFG%_totals_2019'] = filtered_empties_2019_and_2020['eFG%_totals_2019'].astype(float)
-filtered_empties_2019_and_2020['eFG%_totals_2020'] = filtered_empties_2019_and_2020['eFG%_totals_2020'].astype(float)
-filtered_empties_2019_and_2020['WS/48_2019'] = filtered_empties_2019_and_2020['WS/48_2019'].astype(float)
-filtered_empties_2019_and_2020['WS/48_2020'] = filtered_empties_2019_and_2020['WS/48_2020'].astype(float)
-filtered_empties_2019_and_2020['OWS_2019'] = filtered_empties_2019_and_2020['OWS_2019'].astype(float)
-filtered_empties_2019_and_2020['OWS_2020'] = filtered_empties_2019_and_2020['OWS_2020'].astype(float)
+    # Useful for debugging purposes:
+    i = 0
+    for col_name in filtered_empties.columns:
+        print(str(i) + ":")
+        print(col_name)
+        i += 1
+    print(filtered_empties.head(10))
 
+    filtered_empties[filtered_empties.columns[0:6]] = filtered_empties[filtered_empties.columns[0:6]].astype(float)
+    filtered_empties[filtered_empties.columns[7:29]] = filtered_empties[filtered_empties.columns[7:29]].astype(float)
+    filtered_empties[filtered_empties.columns[30:52]] = filtered_empties[filtered_empties.columns[30:52]].astype(float)
+    filtered_empties[filtered_empties.columns[53:74]] = filtered_empties[filtered_empties.columns[53:74]].astype(float)
+    filtered_empties[filtered_empties.columns[75:90]] = filtered_empties[filtered_empties.columns[75:90]].astype(float)
+    filtered_empties[filtered_empties.columns[91:93]] = filtered_empties[filtered_empties.columns[91:93]].astype(float)
+    filtered_empties[filtered_empties.columns[98:112]] = filtered_empties[filtered_empties.columns[98:112]].astype(float)
+    filtered_empties[filtered_empties.columns[113:117]] = filtered_empties[filtered_empties.columns[113:117]].astype(float)
+    filtered_empties[filtered_empties.columns[118:128]] = filtered_empties[filtered_empties.columns[118:128]].astype(float)
+    filtered_empties[filtered_empties.columns[129:151]] = filtered_empties[filtered_empties.columns[129:151]].astype(float)
+    filtered_empties[filtered_empties.columns[152:174]] = filtered_empties[filtered_empties.columns[152:174]].astype(float)
+    filtered_empties[filtered_empties.columns[175:196]] = filtered_empties[filtered_empties.columns[175:196]].astype(float)
+    filtered_empties[filtered_empties.columns[197:212]] = filtered_empties[filtered_empties.columns[197:212]].astype(float)
+    filtered_empties[filtered_empties.columns[213:215]] = filtered_empties[filtered_empties.columns[213:215]].astype(float)
+    filtered_empties[filtered_empties.columns[219:233]] = filtered_empties[filtered_empties.columns[219:233]].astype(float)
+    filtered_empties[filtered_empties.columns[234:238]] = filtered_empties[filtered_empties.columns[234:238]].astype(float)
+    filtered_empties[filtered_empties.columns[239:243]] = filtered_empties[filtered_empties.columns[239:243]].astype(float)
 
+    filtered_by_FG = filtered_empties[filtered_empties["FG_totals_" + str(year1)] >= minFG_year1]
+    filtered_by_FG = filtered_by_FG[filtered_by_FG["FG_totals_" + str(year2)] >= minFG_year2]
 
-filtered_by_FG_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FG_totals_2019 >= 200]
-filtered_by_FG_2019_and_2020 = filtered_by_FG_2019_and_2020[filtered_by_FG_2019_and_2020.FG_totals_2020 >= 150]
-
-print(filtered_by_FG_2019_and_2020.head(10))
-print("shape of data: ", filtered_by_FG_2019_and_2020.shape)
-
-# xlabel = 'eFG%_totals_2019'
-# ylabel = 'TS%_2019'
-xlabel = 'TS%_2019'
-ylabel = 'TS%_2020'
+    return filtered_by_FG
 
 
-# model = sm.OLS(filtered_by_FG_2019_and_2020[ylabel], sm.add_constant(np.column_stack((filtered_by_FG_2019_and_2020['3PAr_2019'], filtered_by_FG_2019_and_2020['FTr_2019'], filtered_by_FG_2019_and_2020['2P%_totals_2019'], filtered_by_FG_2019_and_2020['FT%_totals_2019'], filtered_by_FG_2019_and_2020['TS%_2019']))))
-model = sm.OLS(filtered_by_FG_2019_and_2020[ylabel], sm.add_constant(np.column_stack((filtered_by_FG_2019_and_2020['3PAr_2019'], filtered_by_FG_2019_and_2020['FTr_2019'], filtered_by_FG_2019_and_2020['TS%_2019']))))
-model = sm.OLS(filtered_by_FG_2019_and_2020[ylabel], sm.add_constant(np.column_stack((filtered_by_FG_2019_and_2020['3PAr_2019'], filtered_by_FG_2019_and_2020['FTr_2019'], filtered_by_FG_2019_and_2020['2P%_totals_2019'], filtered_by_FG_2019_and_2020['3P%_totals_2019'], filtered_by_FG_2019_and_2020['FT%_totals_2019'], filtered_by_FG_2019_and_2020['TS%_2019']))))
+#
+# merged_2019_and_2020 = years_stats_dict[2019].merge(years_stats_dict[2020], left_on='playerID', right_on='playerID',
+#           suffixes=('_2019', '_2020'))
+#
+# filtered_empties_2019_and_2020 = merged_2019_and_2020[merged_2019_and_2020.FGA_totals_2019 != str(0)]
+# filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FGA_totals_2020 != str(0)]
+# filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2019 != str(0)]
+# filtered_empties_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FTA_totals_2020 != str(0)]
+#
+# filtered_empties_2019_and_2020['FG_totals_2019'] = filtered_empties_2019_and_2020['FG_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['FG_totals_2020'] = filtered_empties_2019_and_2020['FG_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['TS%_2019'] = filtered_empties_2019_and_2020['TS%_2019'].astype(float)
+# filtered_empties_2019_and_2020['TS%_2020'] = filtered_empties_2019_and_2020['TS%_2020'].astype(float)
+# filtered_empties_2019_and_2020['FG%_totals_2019'] = filtered_empties_2019_and_2020['FG%_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['FG%_totals_2020'] = filtered_empties_2019_and_2020['FG%_totals_2020'].astype(float)
+# # filtered_empties_2019_and_2020['3P%_totals_2019'] = filtered_empties_2019_and_2020['3P%_totals_2019'].astype(float)
+# # filtered_empties_2019_and_2020['3P%_totals_2020'] = filtered_empties_2019_and_2020['3P%_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['2P%_totals_2019'] = filtered_empties_2019_and_2020['2P%_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['2P%_totals_2020'] = filtered_empties_2019_and_2020['2P%_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['FT%_totals_2019'] = filtered_empties_2019_and_2020['FT%_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['FT%_totals_2020'] = filtered_empties_2019_and_2020['FT%_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['3PAr_2019'] = filtered_empties_2019_and_2020['3PAr_2019'].astype(float)
+# filtered_empties_2019_and_2020['3PAr_2020'] = filtered_empties_2019_and_2020['3PAr_2020'].astype(float)
+# filtered_empties_2019_and_2020['FTr_2019'] = filtered_empties_2019_and_2020['FTr_2019'].astype(float)
+# filtered_empties_2019_and_2020['FTr_2020'] = filtered_empties_2019_and_2020['FTr_2020'].astype(float)
+# filtered_empties_2019_and_2020['USG%_2019'] = filtered_empties_2019_and_2020['USG%_2019'].astype(float)
+# filtered_empties_2019_and_2020['USG%_2020'] = filtered_empties_2019_and_2020['USG%_2020'].astype(float)
+# filtered_empties_2019_and_2020['eFG%_totals_2019'] = filtered_empties_2019_and_2020['eFG%_totals_2019'].astype(float)
+# filtered_empties_2019_and_2020['eFG%_totals_2020'] = filtered_empties_2019_and_2020['eFG%_totals_2020'].astype(float)
+# filtered_empties_2019_and_2020['WS/48_2019'] = filtered_empties_2019_and_2020['WS/48_2019'].astype(float)
+# filtered_empties_2019_and_2020['WS/48_2020'] = filtered_empties_2019_and_2020['WS/48_2020'].astype(float)
+# filtered_empties_2019_and_2020['OWS_2019'] = filtered_empties_2019_and_2020['OWS_2019'].astype(float)
+# filtered_empties_2019_and_2020['OWS_2020'] = filtered_empties_2019_and_2020['OWS_2020'].astype(float)
+#
+#
+# filtered_by_FG_2019_and_2020 = filtered_empties_2019_and_2020[filtered_empties_2019_and_2020.FG_totals_2019 >= 200]
+# filtered_by_FG_2019_and_2020 = filtered_by_FG_2019_and_2020[filtered_by_FG_2019_and_2020.FG_totals_2020 >= 150]
+
+
+year1 = 2001
+year2 = 2002
+processed_merged_data = merged_years_setup(year1, year2, 200, 200)
+
+print(processed_merged_data.head(10))
+print("shape of data: ", processed_merged_data.shape)
+
+xlabel = 'TS%_' + str(year1)
+ylabel = 'TS%_' + str(year2)
+
+# model = sm.OLS(processed_merged_data[ylabel], sm.add_constant(np.column_stack((processed_merged_data['3PAr_' + str(year1)], processed_merged_data['FTr_' + str(year1)], processed_merged_data['2P%_totals_' + str(year1)], processed_merged_data['FT%_totals_' + str(year1)], processed_merged_data['TS%_' + str(year1)]))))
+model = sm.OLS(processed_merged_data[ylabel], sm.add_constant(np.column_stack((processed_merged_data['3PAr_' + str(year1)], processed_merged_data['FTr_' + str(year1)], processed_merged_data['TS%_' + str(year1)]))))
 results = model.fit()
 print(results.summary())
 
-slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(filtered_by_FG_2019_and_2020[xlabel], filtered_by_FG_2019_and_2020[ylabel])
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(processed_merged_data[xlabel], processed_merged_data[ylabel])
 print("r_value: ", r_value)
 print("r_squared_value: ", r_value ** 2)
 
-filtered_by_FG_2019_and_2020.plot(x=xlabel, y=ylabel, style='o')
+processed_merged_data.plot(x=xlabel, y=ylabel, style='o')
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.show()
+
