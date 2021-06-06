7f8a63e7da400fb2c8fe546746acef791fca7594
Xavi <x.e.loinaz@gmail.com>
2021-06-05 18:04:26 -0700

    moved old .pkl files, started modifying the data proc script

--- new_version/scrape_and_format_data.py
+++ new_version/scrape_and_format_data.py
@@ -5,14 +5,15 @@ import pandas as pd
 
 
 stats_for_each_year = []
-years = list(range(1980, 2021))
+years = list(range(1998, 2021))
 for year in years:
     url_totals = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_totals.html"
     url_per_game = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     url_per_36_min = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_minute.html"
     url_per_100_poss = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_poss.html"
     url_advanced = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_advanced.html"
-    urls = [url_totals, url_per_game, url_per_36_min, url_per_100_poss, url_advanced]
+    url_shooting = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_shooting.html"
+    urls = [url_totals, url_per_game, url_per_36_min, url_per_100_poss, url_advanced, url_shooting]
 
     url_index = 0
     for url in urls:
@@ -44,22 +45,25 @@ for year in years:
             stats_per_36_min = pd.DataFrame(players_stats, columns=headers)
         elif url_index == 3:
             stats_per_100_poss = pd.DataFrame(players_stats, columns=headers)
-        else:
+        elif url_index == 4:
             stats_advanced = pd.DataFrame(players_stats, columns=headers)
+        else:
+            stats_shooting = pd.DataFrame(players_stats, columns=headers)
 
             # Join the dataframes here
             all_player_stats = stats_totals.join(stats_per_game, lsuffix='_totals')\
                 .join(stats_per_36_min, lsuffix='_per_game')\
                 .join(stats_per_100_poss, lsuffix='_per_36_min', rsuffix='_per_100_poss')\
-                .join(stats_advanced, rsuffix='_advanced')
+                .join(stats_advanced, rsuffix='_advanced')\
+                .join(stats_shooting, rsuffix='_shooting')
             # Drop extraneous columns:
             all_player_stats = all_player_stats\
-                .drop(columns=['playerID_totals', 'playerID_per_game', 'playerID_per_36_min', 'playerID_per_100_poss'])\
-                .drop(columns=['Player_totals', 'Player_per_game', 'Player_per_36_min', 'Player_per_100_poss']) \
-                .drop(columns=['Pos_totals', 'Pos_per_game', 'Pos_per_36_min', 'Pos_per_100_poss']) \
-                .drop(columns=['Age_totals', 'Age_per_game', 'Age_per_36_min', 'Age_per_100_poss']) \
-                .drop(columns=['Tm_totals', 'Tm_per_game', 'Tm_per_36_min', 'Tm_per_100_poss']) \
-                .drop(columns=['G_totals', 'G_per_game', 'G_per_36_min', 'G_per_100_poss']) \
+                .drop(columns=['playerID_totals', 'playerID_per_game', 'playerID_per_36_min', 'playerID_per_100_poss', 'playerID_shooting'])\
+                .drop(columns=['Player_totals', 'Player_per_game', 'Player_per_36_min', 'Player_per_100_poss', 'Player_shooting']) \
+                .drop(columns=['Pos_totals', 'Pos_per_game', 'Pos_per_36_min', 'Pos_per_100_poss', 'Pos_shooting']) \
+                .drop(columns=['Age_totals', 'Age_per_game', 'Age_per_36_min', 'Age_per_100_poss', 'Age_shooting']) \
+                .drop(columns=['Tm_totals', 'Tm_per_game', 'Tm_per_36_min', 'Tm_per_100_poss', 'Tm_shooting']) \
+                .drop(columns=['G_totals', 'G_per_game', 'G_per_36_min', 'G_per_100_poss', 'G_shooting']) \
                 .drop(columns=['GS_totals', 'GS_per_game', 'GS_per_36_min', 'GS_per_100_poss'])
 
         url_index += 1
